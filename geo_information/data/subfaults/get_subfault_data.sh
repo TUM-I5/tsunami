04466fefc6d74fcb7c2335bd0cc34ee5cc96ca25
Martin Schreiber <martin.schreiber@in.tum.de>
2013-12-30 11:27:45 +0100

    fixed and updated subfault script (chile, tohoku)

---
+++ geo_information/data/subfaults/get_subfault_data.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# tohoku usgs
+wget http://earthquake.usgs.gov/earthquakes/eqinthenews/2011/usc0001xgp/results/static_out -O 2011_03_11_tohoku_usgs.txt 
+
+# tohoku ucsb3
+wget http://www.geol.ucsb.edu/faculty/ji/big_earthquakes/2011/03/0311_v3/result_c/static_out -O 2011_03_11_tohoku_ucsb3.txt
+
+# chile usgs
+wget http://earthquake.usgs.gov/earthquakes/eqinthenews/2010/us2010tfan/results2/static_out -O 2010_02_27_chile_usgs.txt
